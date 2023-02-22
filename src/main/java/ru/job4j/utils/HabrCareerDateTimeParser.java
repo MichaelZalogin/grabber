a2916074e7cb7c46b57267f4b05440e6d5fcc0ed
MichaelZalogin <zaloginmiha@gmail.com>
2023-02-23 02:49:52 +0300

    Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,16 @@
+package ru.job4j.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HabrCareerDateTimeParser implements DateTimeParser {
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter formatterForParse = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        LocalDateTime dateTime = LocalDateTime.parse(parse, formatterForParse);
+        DateTimeFormatter formatterOutput = DateTimeFormatter.BASIC_ISO_DATE;
+        dateTime.format(formatterOutput);
+        return dateTime;
+    }
+}
---
+++ src/test/java/ru/job4j/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void localDateParserTest() {
+        String outText = "2023-02-22T17:27:02+03:00";
+        String expected = "2023-02-22T17:27:02";
+        var localDateTime = new HabrCareerDateTimeParser().parse(outText);
+        assertThat(expected).isEqualTo(localDateTime.toString());
+    }
+}

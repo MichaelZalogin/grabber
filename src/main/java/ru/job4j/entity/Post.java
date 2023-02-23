e5344f586f25ae66e9f59d59eb1b6d856fce0868
MichaelZalogin <zaloginmiha@gmail.com>
2023-02-23 03:14:35 +0300

    Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/entity/Post.java
@@ -0,0 +1,30 @@
+package ru.job4j.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+
+    private int id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && Objects.equals(link, post.link);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link);
+    }
+}
